784d7253918687a4bc24293d25c57a9546364f97
JoPed <jonasbjoern@gmail.com>
2023-03-02 13:35:38 +0100

    updated text and footer

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -5,47 +5,53 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faHome, faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faHome } from '@fortawesome/free-solid-svg-icons';
 
 import { faFacebookSquare, faTwitterSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
-        <footer className="footer">
+        <div className="footerContainer container-fluid">
 
-            <Row>
+            <footer className='footer container-lg'>
+                <Row>
                     <Col xs={ 12 } sm={ 6 }>
 
-                    <section className="contactInfo">
+                        <section className="contactInfo">
 
-                        <h2>Kontakt info</h2>
-                        <ul>
-                            <li>Jonas Bjørn Pedersen</li>
-                            <li><FontAwesomeIcon icon={ faEnvelope } /> <a target="_blank" href="mailto:jonasbjoern@gmail.com"> jonasbjoern@gmail.com</a></li>
-                            <li><FontAwesomeIcon icon={ faEnvelope } /> <a target="_blank" href="mailto:jona09h3@videndjurs.net"> jona09h3@videndjurs.net</a> </li>
-                            <li><FontAwesomeIcon icon={ faHome } /> Skolestræde 1, 2.tv, <br />8500 Grenaa</li>
-                        </ul>
+                            <h2>Kontakt info</h2>
+                            <ul>
+                                <li>Jonas Bjørn Pedersen</li>
+                                <li><FontAwesomeIcon icon={ faEnvelope } /> <a target="_blank" href="mailto:jonasbjoern@gmail.com"> jonasbjoern@gmail.com</a></li>
+                                <li><FontAwesomeIcon icon={ faEnvelope } /> <a target="_blank" href="mailto:jona09h3@videndjurs.net"> jona09h3@videndjurs.net</a> </li>
+                                <li><FontAwesomeIcon icon={ faHome } /> Skolestræde 1, 2.tv, <br /><span>8500 Grenaa</span></li>
+                            </ul>
 
-                    </section>
+                        </section>
 
-                </Col>
-                <Col xs={ 12 } sm={ 6 } >
+                    </Col>
+                    <Col xs={ 12 } sm={ 6 } >
 
-                    <section className="socials">
-                        <h2>Links</h2>
-                        <Row className="mb-2 mt-2">
-                            <Col>
-                                <a href="https://www.linkedin.com/in/joped" target="_blank"> <FontAwesomeIcon icon={ faLinkedin } /> </a>
-                                <a href="https://twitter.com/JoPed93" target="_blank"> <FontAwesomeIcon icon={ faTwitterSquare } /> </a>
-                                <a href="https://www.facebook.com/profile.php?viewas=100000686899395&id=1468180457" target="_blank"> <FontAwesomeIcon icon={ faFacebookSquare } />  </a>
-                            </Col>
-                        </Row>
+                        <section className="socials">
+                            <h2>Links</h2>
+                            <Row className="mb-2 mt-2">
+                                <Col>
+                                    <a href="https://github.com/JoPed" target="_blank"> <FontAwesomeIcon icon={ faGithub } /> </a>
+                                    <a href="https://www.linkedin.com/in/joped" target="_blank"> <FontAwesomeIcon icon={ faLinkedin } /> </a>
+                                    <a href="https://twitter.com/JoPed93" target="_blank"> <FontAwesomeIcon icon={ faTwitterSquare } /> </a>
+                                    <a href="https://www.facebook.com/profile.php?viewas=100000686899395&id=1468180457" target="_blank"> <FontAwesomeIcon icon={ faFacebookSquare } />  </a>
+                                </Col>
+                            </Row>
 
-                    </section>
+                        </section>
 
-                </Col>
-            </Row>
-        </footer>
+                    </Col>
+                </Row>
+            </footer>
+
+
+        </div>
     )
 }
 
--- src/assets/components/content/About/AboutContent.js
+++ src/assets/components/content/About/AboutContent.js
@@ -1,6 +1,6 @@
 export const aboutContent = {
     
-        headline: "Velkommen til mit portfolio",
+        headline: "Velkommen til min portfolio",
         paragraphs: ["Jeg er en ung mand på 29 år, der bor i Grenaa hvor jeg studerer webudvikling på Viden Djurs Medie. Jeg har altid været interesseret i IT, dog var det i mange år i form af gaming og opgradering af udstyr.\n\n", "Sidenhen opstod også en glæde ved programmering, som resulterede i, at jeg i 2014 startede på datamatikeruddannelsen i Grenaa, som jeg afsluttede i 2017.\n\n", "Undervejs startede jeg et spil firma med fire medstuderende som følge af vores iværksætter praktik på datamatikeruddannelsen – vi lukkede dog ned igen omkring et år senere. \n\n", "Fra 2017 indtil jeg i januar 2022 begyndte som webudvikler, havde jeg holdt en pause fra programmering hvor jeg i stedet bl.a. arbejdede i Netto som salgsleder.\n\n", "I uddannelsen som webudvikler kommer jeg bl.a. til at arbejde med Responsivt design, JavaScript, React, Bootstrap, Adobe Photoshop, Adobe XD samt meget mere."],
         portraitImgXl: "/assets/images/desktop/about/Portrait_Jonas_Bjoern_Pedersen_xl.webp",
         portraitImgLg: "/assets/images/desktop/about/Portrait_Jonas_Bjoern_Pedersen_lg.webp",
